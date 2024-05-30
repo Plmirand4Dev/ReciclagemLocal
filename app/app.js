c05cad2d75003808775873e2b9dcf907f92265cb
Lucas205 <maxx96072@gmail.com>
2024-05-29 22:11:22 -0300

    as

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@
 import { StatusBar } from "expo-status-bar";
 import { NativeBaseProvider } from "native-base";
 // import { UserProvider } from "./src/contexts/UserContext";
-import { Routes } from "./routes/routes";
+import { Routes } from "../routes/routes";
 import { useEffect, useState } from "react";
 import { LogBox } from "react-native";
 import auth from "@react-native-firebase/auth";
